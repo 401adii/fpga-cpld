7965dfdde81eacdd9bfc287515f2db54bd5a0c44
Adrian Czekański <126412203+401adii@users.noreply.github.com>
2025-10-01 09:56:17 +0200

    first task

---
+++ lab_1/lab_1.srcs/sources_1/new/top.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           LED : out STD_LOGIC_VECTOR (15 downto 0);
+           AD_MCLK : out STD_LOGIC;
+           AD_LRCK : out STD_LOGIC;
+           AD_SCLK : out STD_LOGIC;
+           AD_SDOUT : in STD_LOGIC;
+           DA_MCLK : out STD_LOGIC;
+           DA_LRCK : out STD_LOGIC;
+           DA_SCLK : out STD_LOGIC;
+           DA_SDIN : out STD_LOGIC);
+end top;
+
+architecture Behavioral of top is
+
+begin
+    
+    main : process(clk100mhz)
+    begin
+        
+    end process;
+
+
+end Behavioral;
